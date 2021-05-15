52ba27b965ca0dcb4ab129be9e7170ebba2e651a
Emily Pozzi <epozzi5@yahoo.com>
2021-05-14 20:17:14 -0500

    readme

--- models/category.js
+++ models/category.js
@@ -2,30 +2,43 @@ const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
 
 // create our Category model
-class Category extends Model {}
+// class Category extends Model {}
+//console.log(Category);
+const Category = sequelize.define("category", {
+  id: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    primaryKey: true,
+    autoIncrement: true
+},
+category_name: {
+  type: DataTypes.STRING,
+  allowNull: false
+}
+});
 
-Category.init(
-  {
-    id: {
-        type: DataTypes.INTEGER,
-        allowNull: false,
-        primaryKey: true,
-        autoIncrement: true
-    }
-  },
-  {
-    category_name: {
-        type: DataTypes.STRING,
-        allowNull: false
-    }
-    },
-{
-    sequelize,
-    timestamps: false,
-    freezeTableName: true,
-    underscored: true,
-    modelName: 'category'
-  }
-);
+// Category.init(
+//   {
+//     id: {
+//         type: DataTypes.INTEGER,
+//         allowNull: false,
+//         primaryKey: true,
+//         autoIncrement: true
+//     }
+//   },
+//   {
+//     category_name: {
+//         type: DataTypes.STRING,
+//         allowNull: false
+//     }
+//     },
+// {
+//     sequelize,
+//     timestamps: false,
+//     freezeTableName: true,
+//     underscored: true,
+//     modelName: 'category'
+//   }
+// );
 
 module.exports = Category;
